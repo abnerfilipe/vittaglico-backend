a0a0ae411046d6ab7c72a08cc335505e135689ff
Filipe Abner <filipeabnersantos@gmail.com>
2025-07-15 01:56:58 -0300

    feat: remove AppController, AppService e testes associados; atualiza AppModule com interceptores e filtros globais

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,40 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClassSerializerInterceptor, ConsoleLogger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CoreModule } from './core/core.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { FiltroDeExcecaoGlobal } from './recursos/filtros/filtro-de-excecao-global';
+import { LoggerGlobalInterceptor } from './recursos/interceptores/logger-global.interceptor';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from './modules/usuario/usuario.module';
 
 @Module({
-  imports: [CoreModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CoreModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+    UsuarioModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: FiltroDeExcecaoGlobal,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerGlobalInterceptor,
+    },
+    ConsoleLogger,
+  ],
 })
 export class AppModule {}
